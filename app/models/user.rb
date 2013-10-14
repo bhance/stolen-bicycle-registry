6ab09816ff0c90bcfa8852cf73d55ad9e4e2b24e
epicodus <student@epicodus.com>
2013-10-14 12:49:15 -0700

    test user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :country, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+
   validates :email, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,16 @@ describe User do
   it { should respond_to :zip }
   it { should respond_to :phone1 }
   it { should respond_to :phone2 }
+  it { should respond_to :encrypted_password }
+
+  it { should validate_presence_of :first_name }
+  it { should validate_presence_of :last_name }
+  it { should validate_presence_of :country }
+  it { should validate_presence_of :city }
+  it { should validate_presence_of :state }
+  it { should validate_presence_of :zip }
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :encrypted_password}
 
   it { should validate_uniqueness_of :email }
 end
