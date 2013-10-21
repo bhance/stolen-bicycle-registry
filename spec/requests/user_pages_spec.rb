c313d088b901bbd68dec645d952639f269043f1e
Michal Kaszubowski & Braden OGuinn <student@epicodus.com>
2013-10-21 14:03:42 -0700

    style front page and bicycle show

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -66,10 +66,10 @@ feature 'User pages' do
     fill_in 'Phone', with: @user3.phone1
     fill_in 'Password (min 8 char.)', with: @user3.password
     fill_in 'Password Confirmation', with: @user3.password
-    save_screenshot('tmp/before.png')
+    
     click_button 'Sign up'
     uri = URI.parse(current_url)
-    "#{uri.path}".should == new_bicycle_path
+    "#{uri.path}".should == "/bicycles/new"
   end
 
   scenario 'user selects country \'Canada\' and gets selector to specify a province' do
