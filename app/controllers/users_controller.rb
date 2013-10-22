2b34a048bc07bc989ed5c55c8abfe7fb04c61a78
Abby Ihrig & Niccoli Hersey <student@epicodus.com>
2013-10-22 14:00:28 -0700

    refactor user_spec and create geography_spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,11 @@
 class UsersController < ApplicationController
 
-  def index #fixme remove unneeded methods and views
-  end
-
-  def new
-  end
-
-  def create
-  end
-
   def show
     @user = User.find(params[:id])
     unless current_user == @user
       flash[:alert] = "Access denied"
       redirect_back_or user_path
     end
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
   end
 
 end
