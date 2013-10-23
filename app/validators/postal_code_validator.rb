e70df9bc0314861f34332c77a96816d5f2191e85
Abby Ihrig & Niccoli Hersey <student@epicodus.com>
2013-10-22 17:08:09 -0700

    change error message output

--- app/validators/postal_code_validator.rb
+++ app/validators/postal_code_validator.rb
@@ -1,9 +1,9 @@
 class PostalCodeValidator < ActiveModel::Validator
   def validate(record)
     if record.in_us? && !(/^\d{5}(-\d{4})?$/.match(record.postal_code))
-      record.errors.add(:postal_code, "Postal code must be five numbers")
+      record.errors.add(:postal_code, "must be five numbers")
     elsif record.in_canada? && !(/^\D{1}\d{1}\D{1}(\s|-)?\d{1}\D{1}\d{1}$/.match(record.postal_code))
-      record.errors.add(:postal_code, "Postal code must be in this format 'A0A 0A0'")
+      record.errors.add(:postal_code, "must be in this format 'A0A 0A0'")
     end
   end
 end
