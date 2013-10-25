17772e108758426731333f8aeaa0ff7af3abfd42
Michal Kaszubowski & Dan Griffin <student@epicodus.com>
2013-10-24 17:28:15 -0700

    bike form fixes in phone view

--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,6 @@ ActiveRecord::Schema.define(version: 20131024181506) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "user_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
