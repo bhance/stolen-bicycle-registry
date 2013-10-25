0cac33ff7b894bbc6111ec9d768284d0a5afff45
epicodus <student@epicodus.com>
2013-10-25 14:26:55 -0700

    make dedicated js files for dynamic pages

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,5 @@
 $(function() {
-  $('.alert-success').hide();
-  $('#status-update').click(function() {
-    $('.alert-success').empty().append('Bicycle status updated').show();
-    setTimeout(function(){$('.alert-success').fadeOut()},3000);
+  $('.home-info').click(function() {
+    document.location.href = $(this).attr('rel');
   });
 });
--- app/controllers/bicycles_controller.rb
+++ app/controllers/bicycles_controller.rb
@@ -13,7 +13,8 @@ class BicyclesController < ApplicationController
 
   def new
     if signed_in?
-      @bicycle = Bicycle.new(region: current_user.region, city: current_user.city, country: current_user.country)
+      @bicycle = Bicycle.new(region: current_user.region, city: current_user.city,
+                                                   country: current_user.country)
     else
       redirect_to sign_in_path
     end
