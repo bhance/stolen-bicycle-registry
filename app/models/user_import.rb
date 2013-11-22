bf577599f59793f648d50ecb9c7936f32578c490
Braden OGuinn <bgoguinn@gmail.com>
2013-11-21 17:52:29 -0800

    add user import CSV WIP

--- app/models/user_import.rb
+++ app/models/user_import.rb
@@ -1,10 +1,11 @@
 class UserImport
+  attr_accessor :csv
 
   def initialize(file)
+    csv = [['oid', 'id']]
     rows = SmarterCSV.process(file)
     rows.each do |row|
       row[:postal_code] = row[:postal_code].to_s
-      binding.pry
       User.invite!(row)
     end
   end
--- spec/models/user_import_spec.rb
+++ spec/models/user_import_spec.rb
@@ -2,9 +2,22 @@ require 'spec_helper'
 
 describe UserImport do
   describe '#new' do
+    before do
+      @import = UserImport.new(File.join(Rails.root, 'spec/CSVs/good_user_test.csv'))
+    end
+
     it 'adds users from a CSV file' do
-      UserImport.new(File.join(Rails.root, 'spec/CSVs/good_user_test.csv'))
       User.all.count.should eq 1
+    end
+  end
+
+  describe '#csv' do
+    before do
+      @import = UserImport.new(File.join(Rails.root, 'spec/CSVs/good_user_test.csv'))
+    end
+
+    it 'returns a CSV' do
+      @import.csv.should eq []
     end
   end
 end
